70a781f7ea351a476fd8d628ceb25d3e4938b220
Marcon Willian <marconwillian@protonmail.com>
2022-03-26 11:38:49 -0300

    chore 🚧: Add next public api env

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.bethehero.marcon.run/sandbox'
+    baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
 export default api;
